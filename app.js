27cee4cbf6920239e3a59b445e2084360e2a0e28
Nadjib Amar <hello@ebabel.eu>
2016-11-24 20:57:41 +0100

    struggling with materials but the model geometries are imported

--- app.js
+++ app.js
@@ -8,19 +8,24 @@ init();
 animate();
 
 var loader = new THREE.JSONLoader();
-loader.load(
-  './chessboard.json',
-  function (geometry, materials) {
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var object = new THREE.Mesh(geometry, material);
-    scene.add(object);
-  }
-);
+
+var loaderCallback = function (geometry, materials) {
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var object = new THREE.Mesh(geometry, material);
+  scene.add(object);
+};
+
+loader.load('./models/chessboard.json', loaderCallback);
+loader.load('./models/rook.json', loaderCallback);
+loader.load('./models/knight.json', loaderCallback);
+loader.load('./models/bishop.json', loaderCallback);
+loader.load('./models/king.json', loaderCallback);
+loader.load('./models/queen.json', loaderCallback);
+loader.load('./models/pawn.json', loaderCallback);
 
 function init() {
-
   scene = new THREE.Scene();
-  scene.fog = new THREE.FogExp2( 0xcccccc, 0.002 );
+  scene.fog = new THREE.FogExp2( 0x7ec0ee, 0.002 );
 
   renderer = new THREE.WebGLRenderer();
   renderer.setClearColor( scene.fog.color );
@@ -38,58 +43,32 @@ function init() {
   controls.dampingFactor = 0.25;
   controls.enableZoom = false;
 
-  // world
-
-  var geometry = new THREE.CylinderGeometry( 0, 10, 30, 8, 1 );
-  var material =  new THREE.MeshPhongMaterial( { color:0xffffff, shading: THREE.FlatShading } );
-
-  for ( var i = 0; i < 50; i ++ ) {
-
-    var mesh = new THREE.Mesh( geometry, material );
-    mesh.position.x = ( Math.random() - 0.5 ) * 1000;
-    mesh.position.y = ( Math.random() - 0.5 ) * 1000;
-    mesh.position.z = ( Math.random() - 0.5 ) * 1000;
-    mesh.updateMatrix();
-    mesh.matrixAutoUpdate = false;
-    scene.add( mesh );
-
-  }
-
-  // lights
-
+  // Lights.
   light = new THREE.DirectionalLight( 0xffffff );
   light.position.set( 1, 1, 1 );
   scene.add( light );
-
   light = new THREE.DirectionalLight( 0x002288 );
   light.position.set( -1, -1, -1 );
   scene.add( light );
-
   light = new THREE.AmbientLight( 0x222222 );
   scene.add( light );
 
-  //
-
+  // Stats.
   stats = new Stats();
   container.appendChild( stats.dom );
 
-  //
-
+  // Handle windows resize.
   window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function onWindowResize() {
-
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function animate() {
-
   requestAnimationFrame( animate );
 
   controls.update(); // required if controls.enableDamping = true, or if controls.autoRotate = true
@@ -97,11 +76,8 @@ function animate() {
   stats.update();
 
   render();
-
 }
 
 function render() {
-
   renderer.render( scene, camera );
-
 }
